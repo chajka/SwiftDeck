32668e3382e05ef1cb48b590e8cef981f5c5efbf
Чайка <chajka.niconico@gmail.com>
2023-09-19 18:19:15 +0900

    Define WebViewModel.

--- SwiftDeck/WebView.swift
+++ SwiftDeck/WebView.swift
@@ -8,3 +8,15 @@
 import SwiftUI
 import WebKit
 import Combine
+
+class WebViewModel: ObservableObject {
+    @Published var link: String
+    @Published var didFinishLoading: Bool = false
+    @Published var pageTitle: String
+
+    init (link: String, didFinishLoading: Bool = false, pageTitle: String = "") {
+        self.link = link
+        self.pageTitle = ""
+    }// end init
+}// end class WebViewModel
+
