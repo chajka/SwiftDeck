7d50e00f1f493711008f1baaaacb3d50b98eb9fc
Чайка <chajka.niconico@gmail.com>
2023-09-19 18:02:20 +0900

    Initial Commit

---
+++ SwiftDeck/SwiftDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDeckApp.swift
+//  SwiftDeck
+//
+//  Created by Чайка on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
