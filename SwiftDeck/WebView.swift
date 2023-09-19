bc000873d9983c91870cbc40c2ca4ace1f7c67a1
Чайка <chajka.niconico@gmail.com>
2023-09-19 18:21:48 +0900

    define Coordinator class in struct.

--- SwiftDeck/WebView.swift
+++ SwiftDeck/WebView.swift
@@ -21,4 +21,30 @@ class WebViewModel: ObservableObject {
 }// end class WebViewModel
 
 struct WebView: NSViewRepresentable {
+    class Coordinator: NSObject, WKNavigationDelegate {
+        private var viewModel: WebViewModel
+
+        init(_ viewModel: WebViewModel) {
+           //Initialise the WebViewModel
+           self.viewModel = viewModel
+        }
+        
+        public func webView(_: WKWebView, didFail: WKNavigation!, withError: Error) { }
+
+        public func webView(_: WKWebView, didFailProvisionalNavigation: WKNavigation!, withError: Error) { }
+
+        //After the webpage is loaded, assign the data in WebViewModel class
+        public func webView(_ web: WKWebView, didFinish: WKNavigation!) {
+            self.viewModel.pageTitle = web.title!
+            self.viewModel.link = web.url?.absoluteString ?? "http://google.com"
+            self.viewModel.didFinishLoading = true
+        }
+
+        public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) { }
+
+        public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            decisionHandler(.allow)
+        }
+
+    }
 }
